c188f31a9780192572e8802d8f32719fe1d6d38b
Vidiyala Vikas <vv9969@srmist.edu.in>
2026-02-21 00:49:38 +0530

    Feature(UC4):Print OOPS ARRAY

---
+++ src/com.oops banner/PrintOOPSBanner.java
@@ -0,0 +1,11 @@
+public class PrintOOPSBanner{
+	public static void main(String[] args){
+		System.out.println("   ***   " + " " + "   ***   " + " " + "******   " + " " + "  *****  ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**    ** " + " " + " **    **");
+		System.out.println("**     **" + " " + "**     **" + " " + "**     **" + " " + "**       ");
+		System.out.println("**     **" + " " + "**     **" + " " + "**    ** " + " " + "  ****   ");
+		System.out.println("**     **" + " " + "**     **" + " " + "******   " + " " + "      ** ");
+		System.out.println(" **   ** " + " " + " **   ** " + " " + "**       " + " " + "**     **");
+		System.out.println("   ***   " + " " + "   ***   " + " " + "**       " + " " + "  *****  ");
+	}
+}
---
+++ src/com.oops banner/PrintOOPSBannersjoin.java
@@ -0,0 +1,13 @@
+public class PrintOOPSBannersjoin{
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " ******  ", "  *****  "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **    ** ", " **    ** "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **     **", "**       "));
+        System.out.println(String.join(" ", "**     **", "**     **", " **    ** ", "  ****   "));
+        System.out.println(String.join(" ", "**     **", "**     **", " ******   ", "      ** "));
+        System.out.println(String.join(" ", " **   ** ", " **   ** ", " **       ", "**     **"));
+        System.out.println(String.join(" ", "   ***   ", "   ***   ", " **       ", "  *****  "));
+        
+    }
+}
